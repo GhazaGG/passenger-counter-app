1dadfdf7039a7a41413665a273e5be28357f140a
GhazaGG <ghazagg7@gmail.com>
2022-08-04 18:07:36 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+//document.getElementById("count").innerText = 5
+
+let count = 0
+
+console.log(count)
+
+let myAge = 20;
+let dogAge = 7;
+
+let myDogAge = myAge * dogAge;
+
+console.log(myDogAge)
